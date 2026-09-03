6a8110723f6aba2da14a527ec9016b30a2f9076e
adamkulik <hamstapl@gmail.com>
2026-09-03 21:36:08 +0200

    Bump version to 0.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.pocketwormhole.android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
     }
 
     // Release signing is opt-in via Gradle properties (kept out of the repo):
